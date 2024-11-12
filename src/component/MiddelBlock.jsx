7faef557b36e1d9da00d63dfc957652210832870
Ezeora Nnenna Esther <71467633+chiho-5@users.noreply.github.com>
2024-11-12 03:03:33 +0100

    Update MiddelBlock.jsx

--- src/component/MiddelBlock.jsx
+++ src/component/MiddelBlock.jsx
@@ -14,8 +14,9 @@ const MiddelBlock = () => {
     <div style={{ margin: "100px auto" }} data-aos = "fade-up" class='text-center middl container'>
         <h6>{t("titleMidel")}</h6>
         <p> {t("textMidel")} </p>
+        <a href="https://chat.whatsapp.com/E1hNLKxhucc9SR0Sv7ecoe">
         <button class='BTN'>{t("buttonMidel")} </button>
-
+</a>
     </div>
   )
 }
